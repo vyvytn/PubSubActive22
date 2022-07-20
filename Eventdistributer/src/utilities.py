a4f58be73d53f7ba442f0fcb630e154fe1de549e
kalinakalina <33154514+kalinakalina@users.noreply.github.com>
2022-07-20 21:34:57 +0200

    utilities duplicate

--- Eventdistributer/src/utilities.py
+++ Eventdistributer/src/utilities.py
@@ -3,9 +3,14 @@ import json
 import random
 import numpy as np
 import pandas as pd
-import sys
-sys.path.append("..")
-from TestEventGenerator import gen_datetime
+
+
+def gen_datetime(min_year=2022, max_year=datetime.now().year):
+    # generate a datetime in format yyyy-mm-dd hh:mm:ss.000000
+    start = datetime(min_year, 1, 1, 00, 00, 00)
+    years = max_year - min_year + 1
+    end = start + timedelta(days=365 * years)
+    return start + (end - start) * random.random()
 
 
 def open_pub(pi, goal=""):
@@ -21,12 +26,12 @@ def generate_list(event, data):
     pi_id = ""
     time_now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     if (
-            event == "A"
-            or event == "B"
-            or event == "C"
-            or event == "D"
-            or event == "E"
-            or event == "F"
+        event == "A"
+        or event == "B"
+        or event == "C"
+        or event == "D"
+        or event == "E"
+        or event == "F"
     ):
         timelist.append(gen_datetime())
         values = [item for item in data["events"] if item["event_type"] == event]
@@ -81,9 +86,7 @@ def test_for_patternmatch_and(pi, goal, pattern, event, received_events):
         return received_events
 
     if np.in1d(pattern, plain_recieved).all():
-        print(
-            "publish event AND", plain_recieved
-        )
+        print("publish event AND", plain_recieved)
         open_pub(pi, goal)
         return []
     else:
@@ -93,24 +96,27 @@ def test_for_patternmatch_and(pi, goal, pattern, event, received_events):
 # matched incoming complex SEQ-events
 # pi - brokerclient, goal - not exist because of mocking, pattern - subscribed events for goal, event - incoming eventtype, received_events - all relevent events since last matched
 def test_for_patternmatch_seq(pi, pattern, event, df_received_events):
-    if len(df_received_events) == 0 or not df_received_events["eventtype"].str.contains(event["eventtype"]).any():
+    if (
+        len(df_received_events) == 0
+        or not df_received_events["eventtype"].str.contains(event["eventtype"]).any()
+    ):
         df_received_events = pd.concat([df_received_events, pd.DataFrame(event)])
     else:
         return df_received_events
     sorted_df = df_received_events.sort_values("timestamp")
     if (
-            len(df_received_events) > 1
-            and (pattern[0] == sorted_df["eventtype"].iloc[0])
-            and (pattern[1] == sorted_df["eventtype"].iloc[1])
+        len(df_received_events) > 1
+        and (pattern[0] == sorted_df["eventtype"].iloc[0])
+        and (pattern[1] == sorted_df["eventtype"].iloc[1])
     ):
         if len(pattern) == 2:
             print("publish event SEQUENCE", sorted_df["eventtype"])
             open_pub(pi)
             return pd.DataFrame()
         elif (
-                len(pattern) == 3
-                and len(df_received_events) > 2
-                and (pattern[2] == sorted_df["eventtype"].iloc[2])
+            len(pattern) == 3
+            and len(df_received_events) > 2
+            and (pattern[2] == sorted_df["eventtype"].iloc[2])
         ):
             print("publish event SEQUENCE", sorted_df["eventtype"])
             return pd.DataFrame()
@@ -119,14 +125,19 @@ def test_for_patternmatch_seq(pi, pattern, event, df_received_events):
     else:
         return df_received_events
 
+
 # matched incoming complex AND-events
 # pi - brokerclient, goal - event tp be published, pattern - subscribed events for goal, event - incoming eventtype, received_events - all relevent events since last matched
 def test_for_patternmatch_complex_seq(pi, goal, pattern, event, received_events):
     df_recieved_events = pd.DataFrame(received_events)
     df_event = pd.DataFrame(event)
     # check was already recongnised:
-    if (len(df_recieved_events) == 0 or not df_recieved_events["eventtype"].str.contains(
-            df_event["eventtype"].iloc[0]).any()):
+    if (
+        len(df_recieved_events) == 0
+        or not df_recieved_events["eventtype"]
+        .str.contains(df_event["eventtype"].iloc[0])
+        .any()
+    ):
         df_recieved_events = pd.concat([df_recieved_events, df_event])
     else:
         return received_events
@@ -142,7 +153,10 @@ def test_for_patternmatch_complex_seq(pi, goal, pattern, event, received_events)
         )
         df_value_timestamp = pd.concat([df_values["event_values"], test_time], axis=1)
 
-        if df_recieved_events.loc["C", "timestamp"] < df_value_timestamp["timestamp"].iloc[1]:
+        if (
+            df_recieved_events.loc["C", "timestamp"]
+            < df_value_timestamp["timestamp"].iloc[1]
+        ):
             print("publish event SEQUENCE", pattern)
             open_pub(pi, goal)
             return pd.DataFrame()
